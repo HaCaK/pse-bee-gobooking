fb9fb8b32563d2533acf584b839fea62cd9f24f4
Hannes Can Kececi <hckjapan@gmail.com>
2023-05-13 10:18:42 +0200

    Copy wait-for-it script directly into production stage

--- src/booking/docker-entrypoint.sh
+++ src/booking/docker-entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 
 # Wait for DB
 if [ -n "$DB_CONNECT" ]; then
-    /go/src/app/wait-for-it.sh "$DB_CONNECT" -t 20
+    ./wait-for-it.sh "$DB_CONNECT" -t 20
 fi
 
 # Wait for property
 if [ -n "$PROPERTY_CONNECT" ]; then
-  /go/src/app/wait-for-it.sh "$PROPERTY_CONNECT" -t 20
+  ./wait-for-it.sh "$PROPERTY_CONNECT" -t 20
 fi
 
 # Run the main container command.
--- src/property/docker-entrypoint.sh
+++ src/property/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # Wait for DB
 if [ -n "$DB_CONNECT" ]; then
-    /go/src/app/wait-for-it.sh "$DB_CONNECT" -t 20
+    ./wait-for-it.sh "$DB_CONNECT" -t 20
 fi
 
 # Run the main container command.
